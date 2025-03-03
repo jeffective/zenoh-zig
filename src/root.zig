d3b801e74a113bdc59d0c5502e5451a391a1d57c
kj4tmp@gmail.com <kj4tmp@gmail.com>
2025-03-02 20:24:24 -0800

    working cimport

--- src/root.zig
+++ src/root.zig
@@ -1,15 +1,5 @@
-//! By convention, root.zig is the root source file when making a library. If
-//! you are making an executable, the convention is to delete this file and
-//! start with main.zig instead.
-const std = @import("std");
-const testing = std.testing;
-
-pub export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
-pub extern fn zc_stop_z_runtime() void;
+pub const c = @cImport(@cInclude("zenoh.h"));
 
 test "basic add functionality" {
-    zc_stop_z_runtime();
+    c.zc_stop_z_runtime();
 }
